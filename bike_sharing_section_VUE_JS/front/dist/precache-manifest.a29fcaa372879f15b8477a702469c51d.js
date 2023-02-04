4a30b471a3e38acc0b6583650f4851ba7fd44967
harsimran29122004 <110727993+harsimran29122004@users.noreply.github.com>
2023-02-04 22:43:34 +0100

    Add files via upload

---
+++ bike_sharing_section_VUE_JS/front/dist/precache-manifest.a29fcaa372879f15b8477a702469c51d.js
@@ -0,0 +1,522 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/.gitkeep"
+  },
+  {
+    "revision": "9bbad4c151ef4ead4ed773315815bfb4",
+    "url": "/SplashScreen.scale-100.png"
+  },
+  {
+    "revision": "85105a9d0050818c9674e1152be14143",
+    "url": "/SplashScreen.scale-125.png"
+  },
+  {
+    "revision": "406985771caecfa7fb975822efbdbe04",
+    "url": "/SplashScreen.scale-150.png"
+  },
+  {
+    "revision": "f57f2cf4e318ce37260080ac7b35133f",
+    "url": "/SplashScreen.scale-200.png"
+  },
+  {
+    "revision": "6c31679b6bf5f4b0dd149229849cd961",
+    "url": "/SplashScreen.scale-400.png"
+  },
+  {
+    "revision": "5ce9e755d258cd862f7d9e5f98ab9f7e",
+    "url": "/Square150x150Logo.scale-100.png"
+  },
+  {
+    "revision": "568bd5fd3010a602ce925240079e12f7",
+    "url": "/Square150x150Logo.scale-125.png"
+  },
+  {
+    "revision": "38ca826f9f1bc731d184943ef6b872ed",
+    "url": "/Square150x150Logo.scale-150.png"
+  },
+  {
+    "revision": "b01d4724e0e2cf9bc8087c210918e526",
+    "url": "/Square150x150Logo.scale-200.png"
+  },
+  {
+    "revision": "5d0827c493331ad76bf3cff0030f1e18",
+    "url": "/Square150x150Logo.scale-400.png"
+  },
+  {
+    "revision": "7caeebc184e296250801378a6cec8a92",
+    "url": "/Square310x310Logo.scale-100.png"
+  },
+  {
+    "revision": "8a53306b1a5e923f4926c5b98f511ca8",
+    "url": "/Square310x310Logo.scale-125.png"
+  },
+  {
+    "revision": "ba1cdace4571ccbdcd2999e110eda682",
+    "url": "/Square310x310Logo.scale-150.png"
+  },
+  {
+    "revision": "c33de44fcbbfd50a5c429fad4208e437",
+    "url": "/Square310x310Logo.scale-200.png"
+  },
+  {
+    "revision": "7aad6763a90ec2adbfc596efa9c00f40",
+    "url": "/Square310x310Logo.scale-400.png"
+  },
+  {
+    "revision": "df01e70cf60e21a0804dfa51bd395ef0",
+    "url": "/Square44x44Logo.scale-100.png"
+  },
+  {
+    "revision": "6332f8bb428ef6f6f0d255f74e5adc81",
+    "url": "/Square44x44Logo.scale-125.png"
+  },
+  {
+    "revision": "a25f17ae6734735c2e985e22e486dce8",
+    "url": "/Square44x44Logo.scale-150.png"
+  },
+  {
+    "revision": "d7eb388d584804f21540584ae808fbcc",
+    "url": "/Square44x44Logo.scale-200.png"
+  },
+  {
+    "revision": "7f0d70e8dc9fa9a3bae705655a1c9d3b",
+    "url": "/Square44x44Logo.scale-400.png"
+  },
+  {
+    "revision": "36b54d52370b668b39857594574b61e2",
+    "url": "/Square44x44Logo.targetsize-16.png"
+  },
+  {
+    "revision": "36b54d52370b668b39857594574b61e2",
+    "url": "/Square44x44Logo.targetsize-16_altform-unplated.png"
+  },
+  {
+    "revision": "b086040efb7762152f13de58a4418f98",
+    "url": "/Square44x44Logo.targetsize-24.png"
+  },
+  {
+    "revision": "b086040efb7762152f13de58a4418f98",
+    "url": "/Square44x44Logo.targetsize-24_altform-unplated.png"
+  },
+  {
+    "revision": "6d9f213ac6cb0035b3bf7df209785689",
+    "url": "/Square44x44Logo.targetsize-256.png"
+  },
+  {
+    "revision": "6d9f213ac6cb0035b3bf7df209785689",
+    "url": "/Square44x44Logo.targetsize-256_altform-unplated.png"
+  },
+  {
+    "revision": "fd4d82ce05b8b6264ab02e59ac375682",
+    "url": "/Square44x44Logo.targetsize-48.png"
+  },
+  {
+    "revision": "fd4d82ce05b8b6264ab02e59ac375682",
+    "url": "/Square44x44Logo.targetsize-48_altform-unplated.png"
+  },
+  {
+    "revision": "a17ea121a4ef40778dffe614f069b0f1",
+    "url": "/Square71x71Logo.scale-100.png"
+  },
+  {
+    "revision": "1a550183e8069cd6d68fb8f8222d82ba",
+    "url": "/Square71x71Logo.scale-125.png"
+  },
+  {
+    "revision": "610863aec79ebc322dc9e3945a1bbee5",
+    "url": "/Square71x71Logo.scale-150.png"
+  },
+  {
+    "revision": "84018092ed5e71d7e43623841ec2e3b9",
+    "url": "/Square71x71Logo.scale-200.png"
+  },
+  {
+    "revision": "686f45146f0937186cce9a896e97365a",
+    "url": "/Square71x71Logo.scale-400.png"
+  },
+  {
+    "revision": "cf87dafe363bebbb576c6db8a30582f6",
+    "url": "/StoreLogo.png"
+  },
+  {
+    "revision": "cf87dafe363bebbb576c6db8a30582f6",
+    "url": "/StoreLogo.scale-100.png"
+  },
+  {
+    "revision": "73800459a480a6c358da5e519bedc692",
+    "url": "/StoreLogo.scale-125.png"
+  },
+  {
+    "revision": "3daafddbc4abde42e1d7f89f3cd822e2",
+    "url": "/StoreLogo.scale-150.png"
+  },
+  {
+    "revision": "d39bfdaa64ed0ec264d5c8084e3907a2",
+    "url": "/StoreLogo.scale-200.png"
+  },
+  {
+    "revision": "3cfeeab2ba23c8427ed8af06f57267ba",
+    "url": "/StoreLogo.scale-400.png"
+  },
+  {
+    "revision": "f21476cb9b1cee11b7916a99e7e044e0",
+    "url": "/Wide310x150Logo.scale-100.png"
+  },
+  {
+    "revision": "2de30eb301ad97529435aac8182c04f5",
+    "url": "/Wide310x150Logo.scale-125.png"
+  },
+  {
+    "revision": "0254f7fa7434660ba472ad9e2496b7f5",
+    "url": "/Wide310x150Logo.scale-150.png"
+  },
+  {
+    "revision": "9bbad4c151ef4ead4ed773315815bfb4",
+    "url": "/Wide310x150Logo.scale-200.png"
+  },
+  {
+    "revision": "f57f2cf4e318ce37260080ac7b35133f",
+    "url": "/Wide310x150Logo.scale-400.png"
+  },
+  {
+    "revision": "b975c146755bb5b60e6a9a7a5708f6cc",
+    "url": "/android-chrome-192x192.png"
+  },
+  {
+    "revision": "bf2f0cd51f660ce702b39e76490fcd4e",
+    "url": "/android-chrome-512x512.png"
+  },
+  {
+    "revision": "36055a684cdb05aba0996e7f0624ab8f",
+    "url": "/android-launchericon-144-144.png"
+  },
+  {
+    "revision": "62489d39f414846abcbf8cea430b94cf",
+    "url": "/android-launchericon-192-192.png"
+  },
+  {
+    "revision": "fd4d82ce05b8b6264ab02e59ac375682",
+    "url": "/android-launchericon-48-48.png"
+  },
+  {
+    "revision": "5a50c3b6485ce7fa968e8453d4dab456",
+    "url": "/android-launchericon-512-512.png"
+  },
+  {
+    "revision": "d44bfb7aada2b4e66abe5d25188fcb34",
+    "url": "/android-launchericon-72-72.png"
+  },
+  {
+    "revision": "a3345c1fe2d33cfa54158946e6fc702e",
+    "url": "/android-launchericon-96-96.png"
+  },
+  {
+    "revision": "68108d34bd8f451031daf9f88c1331a9",
+    "url": "/apple-touch-icon.png"
+  },
+  {
+    "revision": "2bb6081290213e4404d4f3ca6701bd63",
+    "url": "/bicyclist.svg"
+  },
+  {
+    "revision": "e946769b9637bc17ed15933fb3d09788",
+    "url": "/browserconfig.xml"
+  },
+  {
+    "revision": "fd4d82ce05b8b6264ab02e59ac375682",
+    "url": "/chrome-extensionmanagementpage-48-48.png"
+  },
+  {
+    "revision": "36b54d52370b668b39857594574b61e2",
+    "url": "/chrome-favicon-16-16.png"
+  },
+  {
+    "revision": "67f4fd4465ed85f9447cfceb5a461af2",
+    "url": "/chrome-installprocess-128-128.png"
+  },
+  {
+    "revision": "1b5bb1f54bb1a7ecfda3",
+    "url": "/css/app.4a63cc8d.css"
+  },
+  {
+    "revision": "7dadee53a77e679ab171",
+    "url": "/css/chunk-vendors.01cff9e3.css"
+  },
+  {
+    "revision": "d0e60fcc37affefff50b4f03a6d43888",
+    "url": "/favicon-16x16.png"
+  },
+  {
+    "revision": "f67b020d4860b0f004872067dd29ba68",
+    "url": "/favicon-32x32.png"
+  },
+  {
+    "revision": "67f4fd4465ed85f9447cfceb5a461af2",
+    "url": "/firefox-general-128-128.png"
+  },
+  {
+    "revision": "36b54d52370b668b39857594574b61e2",
+    "url": "/firefox-general-16-16.png"
+  },
+  {
+    "revision": "6d9f213ac6cb0035b3bf7df209785689",
+    "url": "/firefox-general-256-256.png"
+  },
+  {
+    "revision": "59e54a195ac7d52df167af6af0f87f9e",
+    "url": "/firefox-general-32-32.png"
+  },
+  {
+    "revision": "fd4d82ce05b8b6264ab02e59ac375682",
+    "url": "/firefox-general-48-48.png"
+  },
+  {
+    "revision": "8fe7f6635496575b44476dafeccf8607",
+    "url": "/firefox-general-64-64.png"
+  },
+  {
+    "revision": "aecd6e4c691003ba5664bccc704af452",
+    "url": "/firefox-general-90-90.png"
+  },
+  {
+    "revision": "67f4fd4465ed85f9447cfceb5a461af2",
+    "url": "/firefox-marketplace-128-128.png"
+  },
+  {
+    "revision": "5a50c3b6485ce7fa968e8453d4dab456",
+    "url": "/firefox-marketplace-512-512.png"
+  },
+  {
+    "revision": "465fae5db3903be71432a0a6d91819b4",
+    "url": "/glyph-marker-dot.png"
+  },
+  {
+    "revision": "4f0283c6ce28e888000e978e537a6a56",
+    "url": "/images/layers-2x.png"
+  },
+  {
+    "revision": "a6137456ed160d7606981aa57c559898",
+    "url": "/images/layers.png"
+  },
+  {
+    "revision": "d95d69fa8a7dfe391399e22c0c45e203",
+    "url": "/images/marker-icon-2x.png"
+  },
+  {
+    "revision": "2273e3d8ad9264b7daa5bdbf8e6b47f8",
+    "url": "/images/marker-icon.png"
+  },
+  {
+    "revision": "44a526eed258222515aa21eaffd14a96",
+    "url": "/images/marker-shadow.png"
+  },
+  {
+    "revision": "52a05e731e61db85e7d96bf3f10d919c",
+    "url": "/img/btn_google_signin_light_normal_web@2x.52a05e73.png"
+  },
+  {
+    "revision": "18fca339a19781aac42e858681aaabdc",
+    "url": "/img/cog.18fca339.svg"
+  },
+  {
+    "revision": "d2e34ada3e6d5684b5da0a4d9169cdf8",
+    "url": "/img/crosshair.d2e34ada.svg"
+  },
+  {
+    "revision": "dc794d265479db36efa05e3ef55bc734",
+    "url": "/img/lightning.dc794d26.svg"
+  },
+  {
+    "revision": "d9fc626cb9bd8888b2d1fd1cd10cc6bf",
+    "url": "/img/logo.d9fc626c.png"
+  },
+  {
+    "revision": "6d93baf600a51ede28badfcc813fa4d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b75c7a3b6ff815a42ed921a182cdc75a",
+    "url": "/ios-appicon-1024-1024.png"
+  },
+  {
+    "revision": "a86078a76163eb90507e6c83b34d3d12",
+    "url": "/ios-appicon-120-120.png"
+  },
+  {
+    "revision": "f0d4ad11c37ff3077f5b9ee2214a7252",
+    "url": "/ios-appicon-152-152.png"
+  },
+  {
+    "revision": "56b37b96bb53e0622c8ced8cbcbce0f4",
+    "url": "/ios-appicon-180-180.png"
+  },
+  {
+    "revision": "3d9987e726dda09ebae3859341e42449",
+    "url": "/ios-appicon-76-76.png"
+  },
+  {
+    "revision": "e98c4a8bca5c835e1edc6ae191f68caa",
+    "url": "/ios-launchimage-1024-768.png"
+  },
+  {
+    "revision": "30cf764ce4ce850dbeaa5ebd9aebe41e",
+    "url": "/ios-launchimage-1242-2208.png"
+  },
+  {
+    "revision": "bf03f7117fdcd50f66a3087280515bfe",
+    "url": "/ios-launchimage-1334-750.png"
+  },
+  {
+    "revision": "8c91abee8f4794c7205f66e765e10e2b",
+    "url": "/ios-launchimage-1536-2048.png"
+  },
+  {
+    "revision": "9233a77bb1f292306393a777d8024009",
+    "url": "/ios-launchimage-2048-1536.png"
+  },
+  {
+    "revision": "c98ad1f109deb09f9d7767e1112a7efd",
+    "url": "/ios-launchimage-2208-1242.png"
+  },
+  {
+    "revision": "52572c4dfaf2bdc032acff727d63cc1d",
+    "url": "/ios-launchimage-640-1136.png"
+  },
+  {
+    "revision": "31c8471a32935d52ca3b7c4202d083db",
+    "url": "/ios-launchimage-640-960.png"
+  },
+  {
+    "revision": "76ced2f08cd972c354c3533b5566b240",
+    "url": "/ios-launchimage-750-1334.png"
+  },
+  {
+    "revision": "4c30d8c35b1a6cb105f3d179abd171dd",
+    "url": "/ios-launchimage-768-1024.png"
+  },
+  {
+    "revision": "1b5bb1f54bb1a7ecfda3",
+    "url": "/js/app.71d78f00.js"
+  },
+  {
+    "revision": "7dadee53a77e679ab171",
+    "url": "/js/chunk-vendors.c2adecac.js"
+  },
+  {
+    "revision": "0df2697e0a54e223cbe6d5b011bb471a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "2931579f1d894475c30bd7f5ad0fbba3",
+    "url": "/mstile-150x150.png"
+  },
+  {
+    "revision": "8f4000c6e10030b044dc32407c9c4700",
+    "url": "/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "b086040efb7762152f13de58a4418f98",
+    "url": "/windows-smallsquare-24-24.png"
+  },
+  {
+    "revision": "812dc853be87da0ecfa3fa4d2521edc5",
+    "url": "/windows-smallsquare-30-30.png"
+  },
+  {
+    "revision": "5eb5c5bcd293150041f8033e86734269",
+    "url": "/windows-smallsquare-42-42.png"
+  },
+  {
+    "revision": "3ca121d23273de32d36f368aab5adffa",
+    "url": "/windows-smallsquare-54-54.png"
+  },
+  {
+    "revision": "d132389a20a858e613376946204caf4a",
+    "url": "/windows-splashscreen-1116-540.png"
+  },
+  {
+    "revision": "9bbad4c151ef4ead4ed773315815bfb4",
+    "url": "/windows-splashscreen-620-300.png"
+  },
+  {
+    "revision": "365f8f97c3eff92414bfc366acbaf35f",
+    "url": "/windows-splashscreen-868-420.png"
+  },
+  {
+    "revision": "a86078a76163eb90507e6c83b34d3d12",
+    "url": "/windows-squarelogo-120-120.png"
+  },
+  {
+    "revision": "5ce9e755d258cd862f7d9e5f98ab9f7e",
+    "url": "/windows-squarelogo-150-150.png"
+  },
+  {
+    "revision": "f34807c8c3fc16cee0eb71ce49fe3c80",
+    "url": "/windows-squarelogo-210-210.png"
+  },
+  {
+    "revision": "75f6586908c15b712e7db7cb4d7b20cb",
+    "url": "/windows-squarelogo-270-270.png"
+  },
+  {
+    "revision": "cf87dafe363bebbb576c6db8a30582f6",
+    "url": "/windows-storelogo-50-50.png"
+  },
+  {
+    "revision": "0a4a78d87d1af976ced462ed28a52f83",
+    "url": "/windows-storelogo-70-70.png"
+  },
+  {
+    "revision": "aecd6e4c691003ba5664bccc704af452",
+    "url": "/windows-storelogo-90-90.png"
+  },
+  {
+    "revision": "632dd8ca77f833e1d1ad2c33d7fa2fd6",
+    "url": "/windowsphone-appicon-106-106.png"
+  },
+  {
+    "revision": "df01e70cf60e21a0804dfa51bd395ef0",
+    "url": "/windowsphone-appicon-44-44.png"
+  },
+  {
+    "revision": "7beb36b6b5ad66092a3ff1625940620b",
+    "url": "/windowsphone-appicon-62-62.png"
+  },
+  {
+    "revision": "5ce9e755d258cd862f7d9e5f98ab9f7e",
+    "url": "/windowsphone-mediumtile-150-150.png"
+  },
+  {
+    "revision": "f34807c8c3fc16cee0eb71ce49fe3c80",
+    "url": "/windowsphone-mediumtile-210-210.png"
+  },
+  {
+    "revision": "ce88ef6fa54bf5660c64412c51251110",
+    "url": "/windowsphone-mediumtile-360-360.png"
+  },
+  {
+    "revision": "7a8c9f998cf13d115b4724df528eeb60",
+    "url": "/windowsphone-smalltile-170-170.png"
+  },
+  {
+    "revision": "a17ea121a4ef40778dffe614f069b0f1",
+    "url": "/windowsphone-smalltile-71-71.png"
+  },
+  {
+    "revision": "915617aa2382b7de3880e7288b086074",
+    "url": "/windowsphone-smalltile-99-99.png"
+  },
+  {
+    "revision": "a86078a76163eb90507e6c83b34d3d12",
+    "url": "/windowsphone-storelogo-120-120.png"
+  },
+  {
+    "revision": "cf87dafe363bebbb576c6db8a30582f6",
+    "url": "/windowsphone-storelogo-50-50.png"
+  },
+  {
+    "revision": "0a4a78d87d1af976ced462ed28a52f83",
+    "url": "/windowsphone-storelogo-70-70.png"
+  }
+]);
